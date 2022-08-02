7f88dae6010b566ac9e669ecd0c2e93f31ccdaed
Nodirbek001 <suyunovnodir22@gmail.com>
2022-08-02 15:31:29 +0500

    Nodirbek

--- src/main/java/uz/b/testfreefly/service/PlaneService.java
+++ src/main/java/uz/b/testfreefly/service/PlaneService.java
@@ -6,6 +6,8 @@ import uz.b.testfreefly.domains.Plane;
 import uz.b.testfreefly.dto.plan.PlaneDTO;
 import uz.b.testfreefly.exceptions.BadRequestException;
 
+import java.util.Optional;
+
 public class PlaneService extends Service<PlaneDAO> {
 
     public PlaneService(PlaneDAO dao) {
@@ -13,11 +15,20 @@ public class PlaneService extends Service<PlaneDAO> {
     }
 
     public void create(@NonNull PlaneDTO planDTO) {
-        if(dao.findPlaneByName(planDTO.getName()).isEmpty()){
+        if(!dao.findPlaneByName(planDTO.getName()).isEmpty()){
             throw new BadRequestException("this name already token");
         }
         Plane plane1=planDTO.toPlane();
         dao.create(plane1);
     }
+
+    public void delete(String name){
+        Optional<Plane> planeByName = dao.findPlaneByName(name);
+        if(planeByName.isEmpty()){
+            throw new BadRequestException("not found plane");
+        }
+        dao.delete(planeByName.get().getId());
+    }
+
 
 }
